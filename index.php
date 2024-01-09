24c21612306173e1ab5f01e6ca8f97cb7e516d7c
JohnQuimson <john.quimson777@gmail.com>
2024-01-09 14:25:00 +0100

    feat: table style

--- index.php
+++ index.php
@@ -5,13 +5,19 @@
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>PHP Hotel</title>
 
+  <!-- my CSS -->
+  <link rel="stylesheet" href="css/style.css">
+  <!-- my CSS -->
+
   <!-- Bootstrap CSS -->
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
   <!-- Bootstrap CSS -->
 
 </head>
 <body>
-  
+ 
+  <main>
+    <h1 class="p-5">PHP Hotel</h1>
 <?php
 
     $hotels = [
@@ -58,5 +64,32 @@
 
 ?>
 
+<hr>
+
+ <table class="table table-dark table-hover m-0">
+        <thead>
+            <tr class="bg-light">
+                <th>Name</th>
+                <th>Description</th>
+                <th>Parking</th>
+                <th>Vote</th>
+                <th>Distance to Center</th>
+            </tr>
+        </thead>
+        <tbody>
+          <!-- uso foreach per iterare tutti gli elementi all'interno dell'array -->
+            <?php foreach ($hotels as $hotel){ ?>
+                <tr>
+                  <!-- all'interno di ogni cella inserisco tutti gli elementi, prendendoli in base alla chiave -->
+                    <td><?php echo $hotel['name']; ?></td>
+                    <td><?php echo $hotel['description']; ?></td>
+                    <td><?php echo $hotel['parking'] ? 'Yes' : 'No'; ?></td>
+                    <td><?php echo $hotel['vote']; ?></td>
+                    <td><?php echo $hotel['distance_to_center']; ?></td>
+                </tr>
+            <?php } ?>
+        </tbody>
+    </table>
+</main>
 </body>
 </html>
